d8e7e27d78df3e8d858f373ab75629214a21a2d4
Ulysses Carlos <ulysses_carlos@protonmail.com>
2022-01-20 23:02:35 -0500

    Properly renamed the files now

--- Chapter05/Chapter05_01.js
+++ Chapter05/Chapter05_01.js
@@ -1,38 +1,38 @@
 /*
  * ------------------------------------------------------------------------------
- * Created by Ulysses Carlos on 01/20/2022 at 10:40 PM
+ * Created by Ulysses Carlos on 01/20/2022 at 10:18 PM
  *
- * Everything.js
- *
- * Analogous to the some method, arrays also have an every method.
- * This one returns true when the given function returns true for every
- * element in the array. In a way, some is a version of the || operator that
- * acts on arrays, and every is like the && operator.
- * 
- * Implement every as a function that takes an array and a
- * predicate function as parameters. Write two versions, one using a
- * loop and one using the some method.
+ * Flattening.js
+ * Use the reduce method in combination with the concat method
+ * to “flatten” an array of arrays into a single array that has all the
+ * elements of the original arrays.
  * ------------------------------------------------------------------------------
  */
 
 
-function every(array, predicate) {
-    for (let item of array) {
-        if (!predicate(item))
-            return false;
+// Basically an accumulator, where combine is a lambda function that combines
+// elements
+function reduce(array, combine, start) {
+    let current = start;
+    for (let element of array) {
+        current = combine(current, element);
     }
-    
-    return true;
-    
+
+    return current;
 }
 
 
 function main() {
-    let list = [2, 3, 4, 5, 6, 7];
-    
-    console.log(`Is every item in this list greater than 3? ${every(list, (item) => item > 3)}`);
-    console.log(`Is every item in this list lower than 8? ${every(list, (item) => item < 8)}`);      
-}
+    let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let lambda = function(a, b) {
+        for (let item of b)
+            a.push(item);
+        return a;
+    };
 
-// Now run the damn thing
+    console.log(reduce(array, lambda, []));
+
+}    
+
+// Run the application
 main();
